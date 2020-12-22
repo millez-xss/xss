506c23db71ed70621e841fa7af4bf7c150920ff5
millez-xss <76475465+millez-xss@users.noreply.github.com>
2020-12-22 21:15:14 +1100

    Create waparl.js

---
+++ waparl.js
@@ -0,0 +1,1 @@
+document.title = 'nah mate not good';document.body.innerHTML = '<h1>Very minor XSS bug</h1><img src=https://media.vanityfair.com/photos/5e39fb2c2e790c0008de3318/16:9/w_1280,c_limit/Baby%2520Yoda%2520Whoa1.JPG>'
